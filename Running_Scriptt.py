03e2d06a8b41b7b5d57fe604f32c7aba977f5138
Mansi Sahu <mansi.sahu@veris.in>
2019-06-11 20:58:54 +0530

    automated running server

---
+++ Running_Scriptt.py
@@ -0,0 +1,12 @@
+import os
+os.chdir("C:\\Users\\veris\\PycharmProjects\\Terminal+ new release testing - 11\\pytestpackage")
+
+os.system("adb kill-server")
+os.system("adb start-server")
+os.system("adb devices")
+#os.system('adb -s fc378d12 shell pm clear com.veristerminal')#os.system("pytest -v -s --html=report.html --self-contained-html test_conftest_Alltestcases.py")
+os.system("pytest -v -s --html=report.html --self-contained-html test_conftest_offlinemode.py")
+#os.system("pytest -v -s --html=report.html --self-contained-html test_conftest_Allcheckout.py")
+#os.system("pytest -v -s --html=report.html --self-contained-html test_conftest_emailTestcases.py")
+#pytest -v -s --html=report.html --self-contained-html test_conftest_Allcheckout.py
+#os.system("pytest -v -s --html=report.html --self-contained-html test_conftest_Multiplecheckins.py")
